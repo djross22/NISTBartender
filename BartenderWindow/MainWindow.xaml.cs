ab4e630b9a7b5775de3f6489a96a54f6bdb6f9bb
Ross <djross@nist.gov>
2019-10-11 11:25:08 -0400

    Add UMI tag length Properties and contols

--- BartenderWindow/MainWindow.xaml.cs
+++ BartenderWindow/MainWindow.xaml.cs
@@ -12,17 +12,115 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.ComponentModel;
 
 namespace BartenderWindow
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        //Property change notification event required for INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string forUmiTagLenStr, revUmiTagLenStr;
+        private int[] forUmiTagLen, revUmiTagLen;
+
+        private string outputText;
+
+        #region Properties Getters and Setters
+        public string OutputText
+        {
+            get { return this.outputText; }
+            set
+            {
+                this.outputText = value;
+                OnPropertyChanged("OutputText");
+            }
+        }
+
+        public string RevUmiTagLenStr
+        {
+            get { return this.revUmiTagLenStr; }
+            set
+            {
+                this.revUmiTagLenStr = value;
+                OnPropertyChanged("RevUmiTagLenStr");
+                SetUmiTagLength(forward: false);
+            }
+        }
+
+        public string ForUmiTagLenStr
+        {
+            get { return this.forUmiTagLenStr; }
+            set
+            {
+                this.forUmiTagLenStr = value;
+                OnPropertyChanged("ForUmiTagLenStr");
+                SetUmiTagLength(forward: true);
+            }
+        }
+        #endregion
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
+        }
+
+        private void SetUmiTagLength(bool forward)
+        {
+            string umiLenStr;
+            int[] umiLenArr;
+            if (forward)
+            {
+                umiLenStr = forUmiTagLenStr;
+            }
+            else
+            {
+                umiLenStr = revUmiTagLenStr;
+            }
+            string[] split = umiLenStr.Split('-');
+            umiLenArr = new int[split.Length];
+            for (int i=0; i<split.Length; i++)
+            {
+                umiLenArr[i] = Int32.Parse(split[i]);
+            }
+            if (split.Length == 2)
+            {
+                if (umiLenArr[1] <= umiLenArr[0])
+                {
+                    MessageBox.Show("Warning: Bad UMI length specification.");
+                    return;
+                }
+            }
+
+            if (forward)
+            {
+                forUmiTagLen = umiLenArr;
+                OutputText += $"Forward UMI tag length: ";
+            }
+            else
+            {
+                revUmiTagLen = umiLenArr;
+                OutputText += $"Reverse UMI tag length: ";
+            }
+
+            foreach (int i in umiLenArr) {
+                OutputText += $"{i}, ";
+            }
+            OutputText += "\n";
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
         }
 
         static string ReverseComplement(string inputSequence)
@@ -92,6 +190,22 @@ namespace BartenderWindow
 
             textRange = new TextRange(reverseRichTextBox.Document.ContentStart, reverseRichTextBox.Document.ContentEnd);
             textRange.Text = reverseSequence;
+        }
+
+        private void forUmiTagLenTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                forUmiTagLenTextBox.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            }
+        }
+
+        private void revUmiTagLenTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                revUmiTagLenTextBox.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            }
         }
 
         private void clearWhiteSpaces()
