c602734c6d319b272a1ce276a3b5bf36f5fd6945
Ross <djross@nist.gov>
2019-10-15 09:24:02 -0400

    Add Hamming and Levenshtein distance as static methods to Parser.cs

--- BarcodeParser/Parser.cs
+++ BarcodeParser/Parser.cs
@@ -70,6 +70,68 @@ namespace BarcodeParser
             return new string(charArray);
         }
 
+        //HammingDistance() and LevenshteinDistance() from: https://www.csharpstar.com/csharp-string-distance-algorithm/
+        public static int HammingDistance(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                throw new Exception("Strings must be equal length");
+            }
+
+            int distance =
+                s.ToCharArray()
+                .Zip(t.ToCharArray(), (c1, c2) => new { c1, c2 })
+                .Count(m => m.c1 != m.c2);
+
+            return distance;
+        }
+
+
+        public static int LevenshteinDistance(string s, string t)
+        {
+            int n = s.Length;
+            int m = t.Length;
+            int[,] d = new int[n + 1, m + 1];
+
+            // Step 1
+            if (n == 0)
+            {
+                return m;
+            }
+
+            if (m == 0)
+            {
+                return n;
+            }
+
+            // Step 2
+            for (int i = 0; i <= n; d[i, 0] = i++)
+            {
+            }
+
+            for (int j = 0; j <= m; d[0, j] = j++)
+            {
+            }
+
+            // Step 3
+            for (int i = 1; i <= n; i++)
+            {
+                //Step 4
+                for (int j = 1; j <= m; j++)
+                {
+                    // Step 5
+                    int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+
+                    // Step 6
+                    d[i, j] = Math.Min(
+                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                        d[i - 1, j - 1] + cost);
+                }
+            }
+            // Step 7
+            return d[n, m];
+        }
+
 
     }
 }
